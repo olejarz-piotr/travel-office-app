cab43039d2312d9e2d286d06ed3b3c82eccde7bb
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-11-20 20:51:03 +0100

    creating delete button and refresh

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [tours, setTours] = useState([]);
 
   const removeTour = (id) => {
-    const newTours = tours.filter((tour) => { tour.id !== id });
+    const newTours = tours.filter((tour) =>  tour.id !== id );
     setTours(newTours);
   }
 
@@ -19,7 +19,6 @@ function App() {
     const tours = await response.json();
     setLoading(false);
     setTours(tours);
-    console.log(tours);
   };
 
   useEffect(() => {
@@ -32,9 +31,19 @@ function App() {
       </main>
     );
   }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button className="btn" onClick={()=>fetchTours()}>Refresh</button>
+      </div>
+    </main>)
+  }
   return (
     <main>
-      <Tours tours={tours}/>
+      <Tours tours={tours} removeTour={removeTour}/>
     </main>
   );
 }
